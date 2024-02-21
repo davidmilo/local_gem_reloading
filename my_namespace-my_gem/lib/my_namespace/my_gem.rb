dffb96fa7adab25bfc7e5bc7d3c35413f62f0ab4
David <asmoo252@gmail.com>
2024-02-21 13:15:22 +0100

    remove requires from gem and gemspec

--- my_namespace-my_gem/lib/my_namespace/my_gem.rb
+++ my_namespace-my_gem/lib/my_namespace/my_gem.rb
@@ -1,13 +1,9 @@
 # frozen_string_literal: true
 
-require "http"
-
-require_relative "my_gem/version"
-
 module MyNamespace
   module MyGem
     def self.do_something
-      return "A"
+      # return "A"
       return "B"
     end
   end
